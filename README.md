94e25043af596aa45d6d7633fe56800d7aff1599
xxbert17 <xbert17@vse.cz>
2021-11-21 21:58:50 +0100

    oprava drobnych chyb

--- README.md
+++
@@ -1,2 +0,0 @@
-# staticke_stranky_4IZ268
-Webové stránky restauraci La Palma
--- js-jidelni_listek+napoje.js
+++
@@ -1,3 +0,0 @@
-function prohodit(element, prvniTrida, druhaTrida) {
-    element.className = element.className == prvniTrida ? druhaTrida : prvniTrida;
-}
